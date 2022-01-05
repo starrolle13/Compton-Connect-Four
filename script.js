3eb5a4eeaa955e674854cc4813be025504955074
starrolle13 <starnesharolle13@gmail.com>
2022-01-05 08:44:53 -0800

    Added background photo

--- script.js
+++ script.js
@@ -82,14 +82,8 @@ function winner() {
     let box = findWinner[y];
     if (box.every((o) => gameClick[o].classList.contains('play1'))) {
       win.innerHTML = 'PLAYER 1 WINS!';
-      alert('Player 1 Wins!');
-      window.location.href =
-        '/Users/starnesharussell/ga_seir/unit1/homework/Connect-Four-Game/welcome.html';
     } else if (box.every((b) => gameClick[b].classList.contains('play2'))) {
       win.innerHTML = 'PLAYER 2 WINS!';
-      alert('Player 2 Wins!');
-      window.location.href =
-        '/Users/starnesharussell/ga_seir/unit1/homework/Connect-Four-Game/welcome.html';
     }
   }
 }
@@ -103,7 +97,7 @@ for (let i = 0; i < gameClick.length; i++) {
     ) {
       if (player == 1) {
         gameClick[i].style.backgroundImage =
-          "url('/Users/starnesharussell/Downloads/Untitled design (19).png')";
+          "url('/Users/starnesharussell/Downloads/Untitled (Circle Sticker) (1).png')";
         document.querySelector('h2').innerHTML = 'PLAYER 2';
         gameClick[i].classList.add('play1');
         play1.push(i);
@@ -111,7 +105,7 @@ for (let i = 0; i < gameClick.length; i++) {
         player = 2;
       } else if (player == 2) {
         gameClick[i].style.backgroundImage =
-          "url('/Users/starnesharussell/Downloads/Untitled design (20).png')";
+          "url('/Users/starnesharussell/Downloads/Untitled (Circle Sticker).png')";
         document.querySelector('h2').innerHTML = 'PLAYER 1';
         gameClick[i].classList.add('play2');
         play2.push(i);
