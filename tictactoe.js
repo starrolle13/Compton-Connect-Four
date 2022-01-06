d7cc38eef54e23f8741026200f887da057be28e6
starrolle13 <starnesharolle13@gmail.com>
2022-01-06 08:17:09 -0800

    added another game

---
+++ tictactoe.js
@@ -0,0 +1,10 @@
+let gameBoard = document.body.querySelector('div');
+let player = 0;
+const winningArray = [123, 456, 789, 147, 258, 369, 357, 159];
+
+////game logic
+function gameChoice() {}
+////game winner
+function winningChoice() {}
+////turn game off
+function turnGameOff() {}
