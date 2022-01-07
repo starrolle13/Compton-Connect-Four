8a315b184135046c10dfcd0e8e895952ac15e057
starrolle13 <starnesharolle13@gmail.com>
2022-01-07 08:10:51 -0800

    Added tictactoe pieces function

--- ticTacToe/tictactoe.js
+++ ticTacToe/tictactoe.js
@@ -1,9 +1,35 @@
-let gameBoard = document.body.querySelector('div');
-let player = 0;
+const board = document.querySelectorAll('div');
+let player = 1;
+let playX = [];
+let playO = [];
 const winningArray = [123, 456, 789, 147, 258, 369, 357, 159];
 
 ////game logic
-function gameChoice() {}
+// function gameChoice() {
+for (let i = 0; i < board.length; i++) {
+  board[i].onclick = () => {
+    console.log(i);
+    if (player == 1) {
+      board[i].style.backgroundImage =
+        "url('/Users/starnesharussell/Downloads/Untitled design (5)/1.png')";
+      // document.querySelector('h2').innerHTML = 'PLAYER 2';
+      board[i].classList.add('play1');
+      playX.push(i);
+      // winner();
+      player = 2;
+    } else if (player == 2) {
+      board[i].style.backgroundImage =
+        "url('/Users/starnesharussell/Downloads/Untitled design (5)/2.png')";
+      // document.querySelector('h2').innerHTML = 'PLAYER 1';
+      board[i].classList.add('play2');
+      playO.push(i);
+      // winner();
+      player = 1;
+    }
+  };
+}
+// }
+// gameChoice();
 ////game winner
 function winningChoice() {}
 ////turn game off
